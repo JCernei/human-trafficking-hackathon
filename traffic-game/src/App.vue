d161d0cc00a3fa4a37d1f72b9767a90e666612f4
JCernei <jonnycernei@gmail.com>
2023-11-24 20:36:25 +0200

    Add game map

---
+++ traffic-game/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="map">
+    <vuevectormap
+      width="1280"
+      height="720"
+      map ="world_merc"
+      :options="{
+        // Map options..
+        // markers: []
+        // markerStyle: {}
+        // etc..
+      }">
+    </vuevectormap>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+
+
+#app {
+  /* height: 100%; */
+  width: 100%;
+  /* padding: 2.5rem;   */
+}
+
+#app > div {
+  margin: auto;
+}
+
+.map {
+  position: absolute;
+  /* width: 1280px; */
+  height: 100%;
+  /* border: 1px solid #000; */
+}
+</style>
